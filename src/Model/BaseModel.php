248eefcd50ca23e7b265e1fe11fee6538235e851
Daniel Melchior <daniel.melchior@interweber.de>
2020-03-10 11:59:30 +0100

    feature: cake-like find method and improved date handling

--- src/Model/BaseModel.php
+++ src/Model/BaseModel.php
@@ -15,6 +15,8 @@ use SimpleORMap;
  */
 class BaseModel extends SimpleORMap {
     protected $dateFields = ['mkdate', 'chdate'];
+    const DB_TZ = "UTC";
+    const TZ = "Europe/Berlin";
 
     public function __construct($id = null)
     {
@@ -26,15 +28,140 @@ class BaseModel extends SimpleORMap {
         $value = parent::getValue($field);
 
         if (in_array($field, $this->dateFields)) {
-            $date = DateTimeImmutable::createFromFormat("Y-m-d H:i:s", $value);
+            if ($value instanceof DateTimeImmutable) {
+                return $value->setTimezone(new \DateTimeZone(self::TZ));
+            }
+
+            $date = DateTimeImmutable::createFromFormat("Y-m-d H:i:s", $value, new \DateTimeZone(self::DB_TZ));
 
             if (!$date) {
+                if ($field == 'mkdate' || $field == 'chdate') {
+                    return new DateTimeImmutable("now", new \DateTimeZone(self::TZ));
+                }
+
                 return null;
             }
 
-            return $date;
+            // TODO: use users preferred timezone
+            return $date->setTimezone(new \DateTimeZone(self::TZ));
         }
 
         return $value;
+    }
+
+    public function setValue($field, $value)
+    {
+        if (in_array($field, $this->dateFields)) {
+            if (is_string($value)) {
+                $value = DateTimeImmutable::createFromFormat("Y-m-d H:i:s", $value, new \DateTimeZone(self::TZ));
+
+                if (!$value) {
+                    $value = null;
+                }
+            }
+
+            if ($value instanceof DateTimeImmutable) {
+                $value = $value->setTimezone(new \DateTimeZone(self::DB_TZ));
+            }
+        }
+
+        return parent::setValue($field, $value);
+    }
+
+    public function store()
+    {
+        foreach ($this->dateFields as $field) {
+            if ($val = $this->getValue($field)) {
+                /** @var DateTimeImmutable $val */
+
+                $this->content[$field] = $val
+                    ->setTimezone(new \DateTimeZone(self::DB_TZ))
+                    ->format('Y-m-d H:i:s');
+            }
+        }
+
+        return parent::store();
+    }
+
+    private static function traverseConditions(array $conditions) {
+        $sql = '';
+        $params = [];
+
+        $numItems = count($conditions);
+        foreach ($conditions as $field => $condition) {
+            $lastItem = --$numItems == 0;
+
+            if (is_numeric($field) || strtolower($field) == 'or' || strtolower($field) == 'and') {
+                $keyword = strtolower($field) == 'or' ? " OR " : " AND ";
+
+                if (!is_array($condition)) {
+                    throw new \InvalidArgumentException($keyword . '-key expects array as value.');
+                }
+
+                ['sql' => $subSql, 'params' => $subParams] = static::traverseConditions($condition);
+
+                $prepend = (bool) $sql;
+                if ($prepend) {
+                    $sql .= $keyword;
+                }
+
+                $indentedSql = preg_replace('/^/m', "\t", $subSql);
+                $sql .= "(\n" . $indentedSql . "\n)";
+
+                if (!$prepend && !$lastItem) {
+                    $sql .= $keyword;
+                }
+
+                $params = array_merge($params, $subParams);
+                continue;
+            }
+
+            $field = trim($field);
+            $split = explode(" ", $field, 1);
+
+            $special_conditions = ['or', 'and'];
+            if (!($split[1] ?? false)) {
+                if (in_array(strtolower($condition), $special_conditions)) {
+                    $comp = "";
+                } else {
+                    $comp = "=";
+                }
+            } else {
+                $condition = $split[0];
+
+                $comp = trim($split[1] ?? '=');
+                $comp_whitelist = ['=', '>=', '<=', '>', '<', '!=', 'in', 'not in', 'is null', 'is not null'];
+                if (!in_array(strtolower($comp), $comp_whitelist)) {
+                    throw new \InvalidArgumentException('unknown sql comparison "' . htmlspecialchars($comp) . '"');
+                }
+            }
+
+            if ($sql) {
+                $sql .= " AND\n";
+            }
+
+            if (strtolower($comp) == 'is null' || strtolower($comp) == 'is not null') {
+                $sql .= sprintf("%s %s", $field, $comp);
+            } else {
+                $sql .= sprintf("%s %s ?", $field, $comp);
+
+                if (
+                    strtolower($comp) == 'in'
+                    || strtolower($comp) == 'not in'
+                ) {
+                    $condition = '(' . join(",", $condition) . ')';
+                }
+
+                $params[] = $condition;
+            }
+        }
+
+        return ['sql' => $sql, 'params' => $params];
+    }
+
+    public static function findWhere(array $conditions) {
+        ['sql' => $sql, 'params' => $params] = static::traverseConditions($conditions);
+
+        return static::findBySQL($sql, $params);
     }
 }
