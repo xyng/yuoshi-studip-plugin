c5238eebe22b64b3526f5aaec6f2079218155425
Daniel Melchior <daniel.melchior@interweber.de>
2020-04-15 03:42:08 +0200

    chore: form validation for card form

--- app/pages/Task/EditTaskContent/EditCardContent/EditCardContent.tsx
+++ app/pages/Task/EditTaskContent/EditCardContent/EditCardContent.tsx
@@ -1,6 +1,23 @@
-import React from "react"
+import React, { useCallback } from "react"
+import * as Yup from "yup"
+import { SubmitHandler } from "@unform/core"
 
 import { EditTaskContentView } from "../EditTaskContent"
+import Input from "../../../../components/Form/Input"
+import TextArea from "../../../../components/Form/Textarea"
+import Button from "../../../../components/Button/Button"
+import ValidatedForm from "../../../../components/Form/ValidatedForm"
+
+const CardContentSchema = Yup.object().shape({
+    contents: Yup.array().of(
+        Yup.object().shape({
+            id: Yup.string().required(),
+            title: Yup.string().required(),
+            content: Yup.string().required(),
+        })
+    ),
+})
+type CardContentData = Yup.InferType<typeof CardContentSchema>
 
 const EditCardContent: EditTaskContentView = ({ editTaskContext }) => {
     const {
@@ -8,61 +25,80 @@ const EditCardContent: EditTaskContentView = ({ editTaskContext }) => {
         contents,
         createContent,
         removeContent,
-        onSave,
+        onModifyAndSave,
         onContentInputChange,
     } = editTaskContext
+
+    const onSubmit = useCallback<SubmitHandler<CardContentData>>(
+        async (value) => {
+            await onModifyAndSave((contents) => {
+                return value.contents.map((content) => {
+                    const origContent = contents.find(
+                        (c) => c.id === content.id
+                    )
+
+                    if (!origContent) {
+                        throw new Error("data integrity is broken")
+                    }
+
+                    return {
+                        ...origContent,
+                        title: content.title,
+                        content: content.content,
+                    }
+                })
+            })
+        },
+        [onModifyAndSave]
+    )
 
     return (
         <div>
             <h1>Karteikarten Aufgabe: {task.getTitle()}</h1>
             <div>
-                <button className="button" onClick={createContent()}>
+                <Button onClick={createContent()}>
                     Karteikarte hinzufügen
-                </button>
-                <button className="button" onClick={onSave}>
-                    Speichern
-                </button>
+                </Button>
+                <Button type="submit">Speichern</Button>
             </div>
-            <form
+            <ValidatedForm
+                validation={CardContentSchema}
+                initialData={{
+                    contents,
+                }}
+                onSubmit={onSubmit}
                 className="default"
-                onSubmit={(event) => event.preventDefault()}
             >
-                {contents.map((content) => {
+                {contents.map((content, index) => {
                     return (
                         <div key={`card-content-${content.id}`}>
-                            <label>
-                                <p>Titel</p>
-                                <input
-                                    type="text"
-                                    value={content.title}
-                                    onChange={onContentInputChange(
-                                        content.id,
-                                        "title"
-                                    )}
-                                />
-                            </label>
-                            <label>
-                                <p>Text</p>
-                                <textarea
-                                    value={content.content}
-                                    onChange={onContentInputChange(
-                                        content.id,
-                                        "content"
-                                    )}
-                                />
-                            </label>
+                            <Input
+                                label=""
+                                name={`contents[${index}].id`}
+                                type="hidden"
+                            />
+                            <Input
+                                label="Titel"
+                                name={`contents[${index}].title`}
+                                type="text"
+                            />
+                            <TextArea
+                                label="Text"
+                                name={`contents[${index}].content`}
+                                onChange={onContentInputChange(
+                                    content.id,
+                                    "content"
+                                )}
+                            />
                             <div>
-                                <button
-                                    className="button"
-                                    onClick={removeContent(content.id)}
-                                >
+                                <Button onClick={removeContent(content.id)}>
                                     Entfernen
-                                </button>
+                                </Button>
                             </div>
                         </div>
                     )
                 })}
-            </form>
+            </ValidatedForm>
         </div>
     )
 }
