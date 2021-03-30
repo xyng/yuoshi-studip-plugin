1264b19213b292214831bd6db68da0e50997410f
JulianDierker <jdierker@uni-osnabrueck.de>
2021-03-30 14:47:35 +0200

    for later: get progress

--- app/helpers/importContentTransformers.ts
+++ app/helpers/importContentTransformers.ts
@@ -56,9 +56,7 @@ const contentTransform: {
     [TaskTypeName.CARD]: function (content) {
         return []
     },
-    [TaskTypeName.TEXT]: function (content) {
-        return []
-    },
+
     [TaskTypeName.CLOZE]: function (data) {
         const { content: contents } = data
         if (!(contents instanceof Array)) {
