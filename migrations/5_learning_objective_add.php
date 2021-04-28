934edd6b8cb856c4a1188c3ea6afc798df0c3f86
JulianDierker <jdierker@uni-osnabrueck.de>
2021-04-28 18:05:12 +0200

    fix mysql syntax error

--- migrations/5_learning_objective_add.php
+++ migrations/5_learning_objective_add.php
@@ -8,21 +8,21 @@ class LearningObjectiveAdd extends Migration
     {
         $db = DBManager::get();
         $db->exec("CREATE TABLE IF NOT EXISTS `yuoshi_learning_objectives` (
-             `id` varchar(32) NOT NULL,
-             `package_id` varchar(32) NOT NULL,
-             `title` varchar(64) DEFAULT NULL,
-             `image` varchar(64) DEFAULT NULL,
-             `description` varchar(255) NOT NULL,
-             `mkdate` datetime NOT NULL DEFAULT current_timestamp(),
-             `chdate` datetime NOT NULL DEFAULT current_timestamp(),
-             PRIMARY KEY (`id`),
-             ADD FOREIGN KEY (`package_id`) REFERENCES `yuoshi_packages` (`id`)
-           ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
+            `id` varchar(32) NOT NULL,
+            `package_id` varchar(32) NOT NULL,
+            `title` varchar(64) DEFAULT NULL,
+            `image` varchar(64) DEFAULT NULL,
+            `description` varchar(255) NOT NULL,
+            `mkdate` datetime NOT NULL DEFAULT current_timestamp(),
+            `chdate` datetime NOT NULL DEFAULT current_timestamp(),
+            PRIMARY KEY (`id`),
+            FOREIGN KEY (`package_id`) REFERENCES `yuoshi_packages` (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
 
         $db->exec("CREATE TABLE IF NOT EXISTS `yuoshi_objectives_users` (
             `user_id` varchar(32) NOT NULL,
             `objective_id` varchar(32) NOT NULL,
-            PRIMARY KEY (`user_id`, `objective_id`),
+            PRIMARY KEY (`user_id`, `objective_id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=latin1");
 
         $db->exec("ALTER TABLE `studip`.`yuoshi_stations` 
