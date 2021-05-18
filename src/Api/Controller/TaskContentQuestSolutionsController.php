affdacf63e4499ff25705455a8b8db7b3979e900
JulianDierker <jdierker@uni-osnabrueck.de>
2021-05-18 12:15:55 +0200

    Revert "fix solve task points2"
    This reverts commit ce40e1bb89342a2cd44e0c436b7c4aed55fc2219.

--- src/Api/Controller/TaskContentQuestSolutionsController.php
+++ src/Api/Controller/TaskContentQuestSolutionsController.php
@@ -201,7 +201,7 @@ class TaskContentQuestSolutionsController extends JsonApiController
 <<<<<<< HEAD
 
         // add points for each correct answer
-        foreach ($correct_answers as $correctAnswer) {
+        foreach ($correct_answers as $idx => $correctAnswer) {
             /** @var UserTaskContentQuestSolutionAnswers $solution */
             // $solution = Hash::extract($userAnswers, "{n}[answer_id=$correctAnswer->id]")[0] ?? null; jdierker: this  temporarily got fixed
             $solution = $userAnswers[0];
