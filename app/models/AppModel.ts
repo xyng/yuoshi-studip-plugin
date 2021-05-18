e0c414a82d871e8aea4decfcb1cc02cbb3f88175
JulianDierker <jdierker@uni-osnabrueck.de>
2021-05-18 13:16:46 +0200

    remove log

--- app/models/AppModel.ts
+++ app/models/AppModel.ts
@@ -9,8 +9,6 @@ export abstract class AppModel<T extends {}> extends Model {
         url.search = ""
         url.hash = ""
         url.pathname = process.env.API_PATH as string
-
-        console.log(url.href)
         return url.href
     }
 
