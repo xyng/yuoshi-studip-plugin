9ade61058436e7020c4e6f54024449a17be16afb
JulianDierker <jdierker@uni-osnabrueck.de>
2021-06-21 16:55:42 +0200

    extend taskdescription

---
+++ migrations/6_task_description_extend.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class TaskDescriptionExtend extends Migration
+{
+    public function up()
+    {
+        $db = DBManager::get();
+        $db->exec("ALTER TABLE `yuoshi_tasks` CHANGE `description` `description` VARCHAR(255)");
+    }
+
+    public function down()
+    {
+        //nothing so far
+    }
+}
