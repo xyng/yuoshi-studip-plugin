5009f544ef347719c718c95cc69949d68944a996
SmokyZone <semau@uos.de>
2021-07-27 15:53:41 +0200

    remove tags from read text task

--- app/pages/Task/EditTaskContent/EditTagContent/EditTagContent.tsx
+++ app/pages/Task/EditTaskContent/EditTagContent/EditTagContent.tsx
@@ -18,21 +18,6 @@ const TagContentSchema = Yup.object().shape({
             id: Yup.string().required(),
             title: Yup.string().required(),
             content: Yup.string().required(),
-            quests: Yup.array()
-                .of(
-                    Yup.object().shape({
-                        id: Yup.string().required(),
-                        answers: Yup.array()
-                            .of(
-                                Yup.object().shape({
-                                    id: Yup.string().required(),
-                                    content: Yup.string().required(),
-                                })
-                            )
-                            .required(),
-                    })
-                )
-                .required(),
         })
     ),
 })
@@ -44,8 +29,6 @@ const EditTagContent: EditTaskContentView = () => {
         contents,
         createContent,
         removeContent,
-        createAnswer,
-        removeAnswer,
         onModifyAndSave,
     } = useEditTaskContext()
 
@@ -66,60 +49,32 @@ const EditTagContent: EditTaskContentView = () => {
         })()
     }, [createContent])
 
-    const createTag = useCallback(
-        (content_id: string) => {
-            const content = contents.find((c) => c.id === content_id)
-
-            if (!content || !content.quests.length) {
-                return
-            }
-
-            return createAnswer(content_id, content.quests[0].id, {
-                is_correct: true,
-            })
-        },
-        [contents, createAnswer]
-    )
-
     const onSubmit = useCallback<SubmitHandler<TagContentData>>(
         async (value) => {
             await onModifyAndSave((contents) => {
                 return value.contents.map((content) => {
                     const origContent = findOrFail(contents, "id", content.id)
 
+                    // findOrFail(origContent.quests, "id")
                     return {
                         ...origContent,
                         title: content.title,
                         content: content.content,
-                        quests: content.quests.map((quest) => {
-                            const origQuest = findOrFail(
-                                origContent.quests,
-                                "id",
-                                quest.id
-                            )
-
-                            return {
+                        quests: [
+                            {
                                 name: "Tags",
                                 question: "Tags",
                                 multiple: true,
                                 requireOrder: false,
                                 customAnswer: false,
-                                ...origQuest,
-                                answers: quest.answers.map((answer) => {
-                                    const origAnswer = findOrFail(
-                                        origQuest.answers,
-                                        "id",
-                                        answer.id
-                                    )
-
-                                    return {
-                                        ...origAnswer,
-                                        content: answer.content,
-                                        is_correct: true,
-                                    }
-                                }),
-                            }
-                        }),
+                                answers: [
+                                    {
+                                        content: " ",
+                                        is_correct: "true",
+                                    },
+                                ],
+                            },
+                        ],
                     }
                 })
             })
@@ -178,65 +133,6 @@ const EditTagContent: EditTaskContentView = () => {
                                 label="Text"
                                 name={`${contentPath}.content`}
                             />
-
-                            {/* content always has at least one quest (probably exactly one */}
-                            <Button onClick={createTag(content.id)}>
-                                Tag hinzufügen
-                            </Button>
-
-                            <div>
-                                {content.quests.map((quest, index) => {
-                                    const questPath = `${contentPath}.quests[${index}]`
-
-                                    return (
-                                        <div key={`tags-${quest.id}`}>
-                                            <Input
-                                                label=""
-                                                name={`${questPath}.id`}
-                                                type="hidden"
-                                            />
-                                            {!quest.answers.length && (
-                                                <span>
-                                                    Sie müssen mindestens einen
-                                                    Tag anlegen.
-                                                </span>
-                                            )}
-                                            {quest.answers.map(
-                                                (answer, index) => {
-                                                    const answerPath = `${questPath}.answers[${index}]`
-                                                    return (
-                                                        <div
-                                                            key={`tags-tag-${answer.id}`}
-                                                        >
-                                                            <Input
-                                                                label=""
-                                                                name={`${answerPath}.id`}
-                                                                type="hidden"
-                                                            />
-                                                            <Input
-                                                                label="Tag"
-                                                                name={`${answerPath}.content`}
-                                                                type="text"
-                                                            />
-
-                                                            <button
-                                                                className="button"
-                                                                onClick={removeAnswer(
-                                                                    content.id,
-                                                                    quest.id,
-                                                                    answer.id
-                                                                )}
-                                                            >
-                                                                Tag Entfernen
-                                                            </button>
-                                                        </div>
-                                                    )
-                                                }
-                                            )}
-                                        </div>
-                                    )
-                                })}
-                            </div>
                         </div>
                     </details>
                 )
